1111a8f76deeb1d157f261fba24e0843eaaac73d
Rishabh Rawat <rishabhrawat1423@gmail.com>
2025-12-23 19:48:16 +0530

    Add files via upload

--- alu.v
+++ alu.v
@@ -1,16 +1,22 @@
 module alu(
 input [3:0]a,b,
 input [2:0]sel,
-output [3:0]y
+output reg [3:0]y
 );
-     assign y = (sel == 3'b000) ? (a + b):
-		(sel == 3'b001) ? (a - b):
-		(sel == 3'b010) ? (a & b):
-		(sel == 3'b011) ? (a / b):
-		(sel == 3'b100) ? (a % b):
-		(sel == 3'b101) ? (a ^ b):
-		(sel == 3'b110) ? (a | b):
-				 ~(a & b);
+
+always @(*)begin
+case(sel)
+	3'b000 : y = (a+b);
+	3'b001 : y = (a-b);
+	3'b010 : y = (a&b);
+	3'b011 : y = ~(a|b);
+	3'b100 : y = ~(a^b);
+	3'b101 : y = (a^b);
+	3'b110 : y = (a|b);
+	3'b111 : y = ~(a&b);
+	default : y = 4'b0000;
+	endcase
+end
 
 endmodule
 
@@ -19,15 +25,13 @@ reg [3:0]a,b;
 reg [2:0]sel;
 wire [3:0]y;
 
-alu dut(.a(a),.b(b),.sel(sel),.y(y));
+alu dut(a,b,sel,y);
 
 initial begin
+    a = 4'b0100;   //4
+    b = 4'b0010;   //2
 
-    a = 4'b0101;   // 5
-    b = 4'b0011;   // 3
-
-	$monitor("time = %0t | a = %b | b = %b | sel = %b | y= %b ",$time,a,b,sel,y);
-
+	$monitor("time=%0t | a=%b | b=%b | sel=%b | y=%b ",$time,a,b,sel,y);
 	sel = 3'b000; #5;
 	sel = 3'b001; #5;
 	sel = 3'b010; #5;
@@ -36,8 +40,7 @@ initial begin
 	sel = 3'b101; #5;
 	sel = 3'b110; #5;
 	sel = 3'b111; #5;
-
 	$finish;
-end  
+end
 
 endmodule
